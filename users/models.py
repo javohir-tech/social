da70ec6436b01cd0769be8abbb71072181ac955a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-01-06 01:26:15 +0500

    update

--- users/models.py
+++ users/models.py
@@ -2,34 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from shared.models import BaseModel
 
-ORDENARY_USER , ADMIN , MANAGER = ('ordenary_user' , 'admin' , 'manager')
-VIA_EMAIL, VIA_PHONE = ('via_email' , 'via_phone')
-NEW , CODE_VERIFED , DONE , PHOTO = ('new' , 'code_verifed' , 'done' , 'photo')
+# choices 
+class UserRoles(models.TextChoices):
+    ORDENARY_USER = "ordinary_user", "Ordinary user"
+    ADMIN = "admin", "Admin"
+    MANAGER = "manager", "Manager"
+    
+class AuthType(models.TextChoices) :
+    VIA_EMAIL = 'via_email' , 'Via Email'
+    VIA_PHONE = 'via_phone' , 'Via Phone'
+    
+class AuthStatus(models.TextChoices) :
+    NEW = 'new' , 'New'
+    CODE_VERIFED = 'code_verified' , 'Code verified'
+    DONE = 'done' , 'Done'
+    PHOTO = 'photo' , 'Photo'
 
-class User(AbstractUser , BaseModel):
-    
-    USER_ROLES = (
-        (ORDENARY_USER , ORDENARY_USER),
-        (ADMIN ,  ADMIN),
-        (MANAGER , MANAGER)
-    )
-    
-    AUTH_TYPE = (
-        (VIA_EMAIL , VIA_EMAIL),
-        (VIA_PHONE , VIA_PHONE)
-    )
-    
-    AUTH_STATUS = (
-        (NEW , NEW),
-        (CODE_VERIFED , CODE_VERIFED),
-        (DONE  , DONE),
-        (PHOTO , PHOTO)
-    )
-    
-    user_role = models.CharField(max_length=31 , choices=USER_ROLES , default=ORDENARY_USER)
-    auth_type = models.CharField(max_length=31 , choices=AUTH_TYPE)
-    auth_status =  models.CharField(max_length=31 , choices=AUTH_STATUS , default=NEW)
-    email = models.EmailField(null=True , blank=True , unique=True)
-    phone_number = models.CharField(max_length=13 , null=True,  blank=True , unique=True)
-    photo = models.ImageField(null=True , blank=True, upload_to='users_photo')
 
+class User(AbstractUser, BaseModel):
+
+    user_role = models.CharField(
+        max_length=31, choices=UserRoles.choices, default=UserRoles.ORDENARY_USER
+    )
+    auth_type = models.CharField(max_length=31, choices=AuthType.choices)
+    auth_status = models.CharField(max_length=31, choices=AuthStatus.choices, default=AuthStatus.NEW)
+    email = models.EmailField(null=True, blank=True, unique=True)
+    phone_number = models.CharField(max_length=13, null=True, blank=True, unique=True)
+    photo = models.ImageField(null=True, blank=True, upload_to="users_photo")
