fc5a538246157576772bc208fdca38dd616fb0f4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-01-14 06:06:28 +0500

    updated

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from shared.models import BaseModel
+from django.core.validators import FileExtensionValidator , MaxLengthValidator
+from django.db.models import UniqueConstraint
 
-# Create your models here.
+User = get_user_model
+
+
+class Post(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    image = models.FileField(
+        validators=FileExtensionValidator(allowed_extensions=["jpeg", "jpg", "png"]),
+        upload_to='post_images' , 
+    )
+    caption = models.TextField(validators=MaxLengthValidator(2000))
+    
+    class Meta :
+        db_table = 'post'
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        
+class PostComment(BaseModel) :
+    author = models.ForeignKey(User , on_delete=models.CASCADE )
+    post = models.ForeignKey(Post , on_delete=models.CASCADE ,  related_name='comments')
+    comment = models.TextField(validators=MaxLengthValidator(3000))
+    parent = models.ForeignKey(
+        'self' ,
+        on_delete=models.CASCADE,
+        null=True, 
+        blank=True
+    )
+    
+    def  __str__(self):
+        return f"comment by {self.author}"
+    
+class PostLike(BaseModel) :
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    post  = models.ForeignKey(Post , on_delete=models.CASCADE)
+    
+    class Meta :
+        constraints = [
+            UniqueConstraint(
+                fields = ['author' , 'post'], 
+                name= 'unique  constraint post like'
+            )
+        ]
+
+class CommentLike(BaseModel) :
+    author = models.ForeignKey(User , on_delete=models.CASCADE )
+    comment = models.ForeignKey(PostComment , on_delete=models.CASCADE )
+    
+    class Meta :
+        constraints = [
+            UniqueConstraint(
+                fields=['author' , 'comment'],
+                name= 'comment like unique'
+            )
+        ]   
+
