f17bb1d6dc8d9f58ee49fc0674dd15db403ef001
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-01-15 19:55:05 +0500

    update

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, CreatePostView, RetriveView
+from .views import PostListView, CreatePostView, RetriveView, CommentListCreateView
 
 urlpatterns = [
     path("posts/", PostListView.as_view()),
     path("create/", CreatePostView.as_view()),
     path("detail/<uuid:pk>/", RetriveView.as_view()),
+    path("<uuid:pk>/comments/", CommentListCreateView.as_view()),
+    path("comments/create/", CommentListCreateView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView , RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny , IsAuthenticatedOrReadOnly
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListCreateAPIView
+)
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+)
 from .models import Post, PostComment, PostLike, CommentLike
 from .serializers import (
     PostSerializer,
@@ -25,50 +34,57 @@ class PostListView(ListAPIView):
 class CreatePostView(CreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(author = self.request.user)
+        serializer.save(author=self.request.user)
         return Response(
             {
-                'success' : True,
-                "message" : "successfully create", 
-                "result" : serializer.data
+                "success": True,
+                "message": "successfully create",
+                "result": serializer.data,
             }
         )
+
 
 class RetriveView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def put(self, request, *args, **kwargs):
-        post  =  self.get_object()
-        serializer = self.serializer_class(post , data = request.data , partial = True)
+        post = self.get_object()
+        serializer = self.serializer_class(post, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(
             {
-                "success" : True,
-                "message" : "successfuly edit",
+                "success": True,
+                "message": "successfuly edit",
                 "code": status.HTTP_200_OK,
-                "result" : serializer.data
+                "result": serializer.data,
             }
         )
-        
+
     def delete(self, request, *args, **kwargs):
         post = self.get_object()
         post.delete()
         return Response(
             {
-                "success"  : True,
-                "message"  : "ochirildi ", 
-                "code" : status.HTTP_204_NO_CONTENT
+                "success": True,
+                "message": "ochirildi ",
+                "code": status.HTTP_204_NO_CONTENT,
             }
         )
-        
-        
 
-             
-    
+
+class CommentListCreateView(ListCreateAPIView):
+    serializer_class = PostCommentSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = PostComment.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+                
